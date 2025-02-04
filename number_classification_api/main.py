d60bbb9a5d2264c81948b738913858f2c757c1d5
agimuche1 <agimuche1@gmail.com>
2025-02-04 06:56:20 +0100

    new import

--- number_classification_api/main.py
+++ number_classification_api/main.py
@@ -1,3 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Number Classification API"}
+
 from fastapi import FastAPI, Query
 from typing import Dict
 
